d6578ca1682a6f15a3b5fdea1976af530c1af42b
iwood25 <petr.ryabokon@gmail.com>
2016-06-08 22:13:01 +0300

    first Commit

---
+++ ContentBuild/js/main.js
@@ -0,0 +1,44 @@
+﻿$(function () {
+	// Owl Carousel
+	var owl = $('.slider');
+	owl.owlCarousel({
+		loop: true,
+		items: 1,
+		itemClass: "slide-wrap",
+		navContainer: "arrow"
+	});
+
+	$(".arrow").click(function () {
+		owl.trigger("next.owl.carousel");
+	});
+
+	// my-menu
+
+	$(".top-menu").after("<div id='my-menu'>");
+	$(".top-menu").clone().appendTo("#my-menu");
+	$("#my-menu").find("*").attr("style", "");
+	$("#my-menu").mmenu({
+		extensions: ['widescreen', 'theme-white', 'effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: "Menu"
+		}
+	});
+
+	var api = $("#my-menu").data("mmenu");
+	api.bind("closed", function () {
+		$(".toggle-mnu").removeClass("on");
+	});
+
+	// Toggle-mnu
+	$(".mobile-mnu").click(function () {
+		var mmAPI = $("#my-menu").data("mmenu");
+		mmAPI.open();
+		var thiss = $(this).find(".toggle-mnu");
+		thiss.toggleClass("on");
+		$(".main-mnu").slideToggle();
+		return false;
+	});
+
+
+	
+});
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', ['sass'], function () {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		},
+		notify: false
+	});
+});
+
+gulp.task('sass', function () {
+	return gulp.src('./ContentBuild/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./Content/css'))
+		.pipe(browserSync.stream());
+});
+
+
+gulp.task('sass:watch', function () {
+	gulp.watch('./ContentBuild/scss/**/*.scss', ['sass']);
+	gulp.watch('./Content/js/*.js').on("change", browserSync.reload);
+	gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync', 'sass:watch']);
